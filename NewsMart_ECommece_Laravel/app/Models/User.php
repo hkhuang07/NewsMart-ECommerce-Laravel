374d4cfe32dafebfb1e97cd32f348ec5ac49309e
phanchihai <phanchihai08102004@gmail.com>
2025-11-23 14:30:48 +0700

    Update email 2

--- NewsMart_ECommece_Laravel/app/Models/User.php
+++ NewsMart_ECommece_Laravel/app/Models/User.php
@@ -145,17 +145,3 @@ class User extends Authenticatable
     }
 }
 
- 
-class CustomResetPasswordNotification extends ResetPassword 
-{ 
-    public function toMail($notifiable) 
-    { 
-        return (new MailMessage) 
-            ->subject('Khôi phục mật khẩu') 
-            ->line('Bạn vừa yêu cầu ' . config('app.name') . ' khôi phục mật khẩu của mình.') 
-            ->line('Liên kết đặt lại mật khẩu này sẽ hết hạn sau 60 phút.') 
-            ->line('Xin vui lòng nhấn vào nút "Khôi phục mật khẩu" bên dưới để tiến hành cấp mật khẩu mới.') 
-            ->action('Khôi phục mật khẩu', url(config('app.url') . route('password.reset', $this->token, false))) 
-            ->line('Nếu bạn không yêu cầu đặt lại mật khẩu, xin vui lòng không làm gì thêm và báo lại cho quản trị hệ thống về vấn đề này.'); 
-    } 
-}
